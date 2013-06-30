4a0b495a682a57a86b10250cde1ed7cd8bf05e26
root <root@milan-pc.(none)>
2013-06-30 10:56:31 +0530

    Updated Version

--- upload.py
+++ upload.py
@@ -8,66 +8,65 @@ import json
 import math
 import time
 import datetime
-from pytz import timezone
 
-S_API_KEY = "3773bd8cf9594ca7a2a6c0074f73ace7"
-D_Loc = "A-Home"
-D_NAME = "Node-AH"
-BASEPATH = 	"/home/milan/Projects/AC/CSVs/"					# [Path of CSV files]
-URL_S = "http://sensoract.iiitd.edu.in:9000/upload/wavesegment"
-now = datetime.datetime.now(timezone('Asia/Kolkata'))
+from pytz import timezone
+from SENSOR_ACT_CONFIG import SA_API_KEY
+from SENSOR_ACT_CONFIG import DEVICE_LOCATION
+from SENSOR_ACT_CONFIG import DEVICE_NAME
+from SENSOR_ACT_CONFIG import URL_SENSOR_ACT
+
+FLYPORT_DATA_BASEPATH =	[Path_To_CSV_Folder]
+# Log File	
+log_file = open(FLYPORT_DATA_BASEPATH + "Log_File.txt","a")
+	
 while(True):
-	loop_lock = 0
-	Log_Filename = "Log_" + str(now.day) + "_" + str(now.month) + "_" + str(now.hour) + "_" + str(now.minute) + ".txt"
-	Log_File = BASEPATH + Log_Filename
-	list_of_files = glob.glob(BASEPATH+str("*.csv"))
-	print list_of_files
+	
+	# Current Unix Timestamp and Datetime
+	observation_timestamp = time.time()
+	observation_datetime = datetime.datetime.fromtimestamp(observation_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+	# List of files to Upload
+	list_of_files = glob.glob(FLYPORT_DATA_BASEPATH+str("*.csv"))
+	log_file.write(str(observation_datetime) + "=> Files To Upload: " + str(list_of_files) + "\n")
+	
+	# For Loop to iterate through every File in the folder
 	for f in list_of_files:
+		# Check if file is in use for data collection
 		if int(time.time())-int(os.stat(f).st_mtime)>900:
 			with open(f) as filein:
 				reader = csv.reader(filein, quoting=csv.QUOTE_NONNUMERIC, skipinitialspace = True)
-				lo=open(Log_File,"a")
-				lo.write("\n"+str(time.time())+"  "+"Starting Upload for File: -"+str(f))
-				lo.close()
-
+				log_file.write(str(observation_datetime) + "=> Starting Upload of File: " + str(f) + "\n")
+				
 				for row in reader:
+					
 					timestamp = math.fabs(row[0])
 					temperature = row[1]
 					pir = row[2]
 					light = row[3]
 					
+					# Create Packet
 					headers = { 'Content-Type': 'application/json; charset=UTF-8'}
-					payload1 = { 'secretkey' : S_API_KEY, "data" : { "loc" : D_Loc, "dname" : str(D_NAME), "sname" :"PIRSensor", "sid" : "1", "timestamp" : timestamp, "channels" : [ { "cname" : "channel1", "unit" : "none","readings" : [pir] } ] } }
-					payload2 = { 'secretkey' : S_API_KEY, "data" : { "loc" : D_Loc, "dname" : str(D_NAME), "sname" :"TemperatureSensor", "sid" : "1", "timestamp" : timestamp, "channels" : [ { "cname" : "channel1", "unit" : "none","readings" : [temperature] } ] } }
-					payload3 = { 'secretkey' : S_API_KEY, "data" : { "loc" : D_Loc, "dname" : str(D_NAME), "sname" :"LightSensor", "sid" : "1", "timestamp" : timestamp, "channels" : [ { "cname" : "channel1", "unit" : "none","readings" : [light] } ] } }
+					payload1 = { 'secretkey' : SA_API_KEY, "data" : { "loc" : DEVICE_LOCATION, "dname" : str(DEVICE_NAME), "sname" :"PIRSensor", "sid" : "1", "timestamp" : timestamp, "channels" : [ { "cname" : "channel1", "unit" : "none","readings" : [pir] } ] } }
+					payload2 = { 'secretkey' : SA_API_KEY, "data" : { "loc" : DEVICE_LOCATION, "dname" : str(DEVICE_NAME), "sname" :"TemperatureSensor", "sid" : "1", "timestamp" : timestamp, "channels" : [ { "cname" : "channel1", "unit" : "none","readings" : [temperature] } ] } }
+					payload3 = { 'secretkey' : SA_API_KEY, "data" : { "loc" : DEVICE_LOCATION, "dname" : str(DEVICE_NAME), "sname" :"LightSensor", "sid" : "1", "timestamp" : timestamp, "channels" : [ { "cname" : "channel1", "unit" : "none","readings" : [light] } ] } }
 
 					try:
-						r1 = requests.post(URL_S, data=json.dumps(payload1), headers=headers)
-						time.sleep(1)
-						r2 = requests.post(URL_S, data=json.dumps(payload2), headers=headers)
-						time.sleep(1)
-						r3 = requests.post(URL_S, data=json.dumps(payload3), headers=headers)
-
-					except Exception as f2:
-						lo=open(Log_File,"a")
-						lo.write("\n"+str(time.time())+"  "+"filename: "+f +"  Error: "+f2.__str__())
-						lo.close()
-						loop_lock = 1
-						break
-					time.sleep(2)
+						# Upload Data
+						r1 = requests.post(URL_SENSOR_ACT, data=json.dumps(payload1), headers=headers)
+						r2 = requests.post(URL_SENSOR_ACT, data=json.dumps(payload2), headers=headers)
+						r3 = requests.post(URL_SENSOR_ACT, data=json.dumps(payload3), headers=headers)
+						
+					except requests.exceptions.ConnectionError:
+						# If Connection Down
+						log_file.write(str(observation_datetime) + "=> Network Down: " + str(f) + "\n")
+						continue
+					
+					except Exception as exception_raised:
+						# Any Other Exception Raised
+						log_file.write(str(observation_datetime) + "=> " + str(exception_raised) + "\n")
+						continue
+				log_file.write(str(observation_datetime) + "=> File Uploaded. Now Deleting \n")
+				os.remove(f)
 		else:
-			lo=open(Log_File,"a")
-			lo.write("\n"+str(time.time())+": File in Use \n"+f)
-			lo.close()
-		time.sleep(1)
-		if loop_lock == 0:
-			lo=open(Log_File,"a")
-			lo.write("\n"+str(time.time())+"  "+"Upload Success. \n Now removing file: "+str(f))
-			lo.close()
-			os.remove(f)
-		else:
-			lo=open(Log_File,"a")
-			lo.write("\n"+str(time.time())+"  "+"Connectivity Issue, Going to sleep ")
-			lo.close()
-			
+			log_file.write(str(observation_datetime) + "=> Data Collection Going On \n")
 	time.sleep(900)
